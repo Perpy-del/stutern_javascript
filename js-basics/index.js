2b5375b78c5369b25c8d1e0b0adb93b83903b3ac
Perpy-del <perpetualmeninwa@gmail.com>
2023-02-09 16:20:02 +0100

    JavaScript Basics

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,15 +1,37 @@
+// Write a function that multiplies two numbers.
+function multiply(x, y) {
+    return x * y;
+}
+
+/* Call the function to a console to multiply any two numbers of your choice and log the answer to the console. */
+
+console.log(multiply(12, 5));
+
+
+// Second Task
+
+/* Create variables for your first name, last name, Stutern track, skills, favourite color, year of your country’s independence. */
+
+// Variable for first name called lastName.
 const firstName = "Perpetual";
 
+// Variable for last name called lastName.
 const lastName = "Meninwa";
 
+// Variable for Stutern track called StuternTrack.
 const StuternTrack = "Backend Development";
 
+// Variable for my skills called Skills.
 const Skills = "HTML and CSS";
 
+// Variable for my favorite color called FavColor
 const FavColor = "Nude";
 
-const countryIndependence = 1960;
+// Variable for my country's independence year called countryIndependenceYear
+const countryIndependenceYear = 1960;
 
-const BE_Assignment = `My name is ${firstName} ${lastName} and I am in the ${StuternTrack} track. My current skills are ${Skills} and my favaorite color is ${FavColor}. My country Nigeria gained independence in ${countryIndependence}.`;
+// Use these variables to form a sentence and put it in a variable called BE_Assignment
+const BE_Assignment = `My name is ${firstName} ${lastName} and I am in the ${StuternTrack} track. My current skills are ${Skills} and my favaorite color is ${FavColor}. My country Nigeria gained independence in ${countryIndependenceYear}.`;
 
+// Now log the sentence to the console.
 console.log(BE_Assignment);
