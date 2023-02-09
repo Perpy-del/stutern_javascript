35d6491940148c9041a9efc8e9827686e69bda7a
Perpy-del <perpetualmeninwa@gmail.com>
2023-02-09 15:58:12 +0100

    Stutern JS Assignment

---
+++ js-basics/index.js
@@ -0,0 +1,15 @@
+const firstName = "Perpetual";
+
+const lastName = "Meninwa";
+
+const StuternTrack = "Backend Development";
+
+const Skills = "HTML and CSS";
+
+const FavColor = "Nude";
+
+const countryIndependence = 1960;
+
+const BE_Assignment = `My name is ${firstName} ${lastName} and I am in the ${StuternTrack} track. My current skills are ${Skills} and my favaorite color is ${FavColor}. My country Nigeria gained independence in ${countryIndependence}.`;
+
+console.log(BE_Assignment);
